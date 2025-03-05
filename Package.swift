c80598b4639d0094fd8af908fc36b991a6a4433a
pthibaud <pthibaud@users.noreply.github.com>
2025-03-05 17:19:12 +0100

    bug: swift package needs some root access

--- Package.swift
+++ Package.swift
@@ -9,10 +9,10 @@ let package: Package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
-        .package(url: "https://github.com/apple/swift-format", from: "508.0.1"),
-        // .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
-        .package(url:"https://github.com/apple/swift-cmark", branch:"gfm"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.0"),
+        //.package(url: "https://github.com/apple/swift-format", from: "600.0.0"),
+        //.package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        //.package(url: "https://github.com/apple/swift-cmark", branch:"gfm"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
