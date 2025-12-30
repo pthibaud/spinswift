808918e8484202139a905a5b5d1aa9a2d24f9d10
pthibaud <pthibaud@users.noreply.github.com>
2025-12-30 21:26:08 +0100

    remove double test

--- Tests/spinswiftTests/spinswiftTests.swift
+++ Tests/spinswiftTests/spinswiftTests.swift
@@ -18,8 +18,6 @@ final class spinswiftTests: XCTestCase {
             Matrix3(fill: "test").inverse()
                 == Matrix3(0, 0, 1 / 9, 6 / 19, -5 / 19, -1 / 57, -1 / 19, 4 / 19, -1 / 19))
         XCTAssertTrue(
-            Matrix3(fill: "test").diagonalPart() == Matrix3(0, 54, -9, 0, -45, 36, 19, -3, -9))
-        XCTAssertTrue(
             Matrix3(fill: "test").adjoint() == Matrix3(0, 0, 19, 54, -45, -3, -9, 36, -9))
         XCTAssertTrue(
             Matrix3(fill: "test").diagonalPart() == Matrix3(3, 0, 0, 0, 1, 0, 0, 0, 0))
